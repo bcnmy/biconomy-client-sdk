f0f18b465686926879be20f6dd0fff674f8aa8d7
talha <talha.malik@ideofuzion.com>
2022-07-04 20:24:14 +0500

    typo fix

--- packages/core-types/src/contracts/SmartWalletFactoryContract.ts
+++ packages/core-types/src/contracts/SmartWalletFactoryContract.ts
@@ -3,8 +3,8 @@ import {
   TransactionResult
 } from '../types'
 export interface SmartWalletFacoryContract {
-    deployCounterFactualWallet(owner:string, entryPointL:string, handler:string, index:number): Promise<TransactionResult>
-    deployWallet(owner:string, entryPointL:string, handler:string): Promise<TransactionResult>
+    deployCounterFactualWallet(owner:string, entryPoint:string, handler:string, index:number): Promise<TransactionResult>
+    deployWallet(owner:string, entryPoint:string, handler:string): Promise<TransactionResult>
     getAddressForCounterfactualWallet(owner:string, index:number): Promise<string>
   }
   
--- packages/ethers-lib/src/contracts/SmartWalletFactory/SmartWalletProxyFactoryEthersContract.ts
+++ packages/ethers-lib/src/contracts/SmartWalletFactory/SmartWalletProxyFactoryEthersContract.ts
@@ -11,13 +11,13 @@ import { SmartWalletFacoryContractSV100Interface } from '../../../typechain/src/
 class SmartWalletFacoryContractEthers implements SmartWalletFacoryContract {
   constructor(public contract: SmartWalletFacoryContract_V1_0_0) {}
 
-  async deployCounterFactualWallet(owner:string, entryPointL:string, handler:string, index:number): Promise<TransactionResult>{
-    const resultSet = await this.contract.deployCounterFactualWallet(owner, entryPointL, handler, index)
+  async deployCounterFactualWallet(owner:string, entryPoint:string, handler:string, index:number): Promise<TransactionResult>{
+    const resultSet = await this.contract.deployCounterFactualWallet(owner, entryPoint, handler, index)
     return toTxResult(resultSet)
   }
 
-  async deployWallet(owner:string, entryPointL:string, handler:string): Promise<TransactionResult>{
-    const resultSet = await this.contract.deployWallet(owner, entryPointL, handler)
+  async deployWallet(owner:string, entryPoint:string, handler:string): Promise<TransactionResult>{
+    const resultSet = await this.contract.deployWallet(owner, entryPoint, handler)
     return toTxResult(resultSet)
   }
 
