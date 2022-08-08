64c34fa95f3ed26bf788cc68d5699e333c5a1152
livingrockrises <chirag@biconomy.io>
2022-08-09 02:53:31 +0530

    refund txn creation + dev notes

--- packages/smart-account/src/SmartAccount.ts
+++ packages/smart-account/src/SmartAccount.ts
@@ -351,6 +351,62 @@ class SmartAccount {
     return walletTx
   }
 
+  // Other helpers go here for pre build (feeOptions and quotes from relayer) , build and execution of refund type transactions 
+
+  /**
+   * Prepares compatible WalletTransaction object based on Transaction Request
+   * @todo Rename based on other variations to prepare transaction
+   * @notice This transaction is with fee refund (smart account pays using it's own assets accepted by relayers)
+   * @param transaction
+   * @param batchId
+   * @param chainId
+   * @returns
+   */
+   async createRefundTransaction(
+    transaction: Transaction,
+    feeToken: string, // review types
+    batchId: number = 0,
+    chainId: ChainId = this.#smartAccountConfig.activeNetworkId
+  ): Promise<WalletTransaction> {
+    let walletContract = this.smartAccount(chainId).getContract()
+    walletContract = walletContract.attach(this.address)
+
+    // NOTE : If the wallet is not deployed yet then nonce would be zero
+    let nonce = 0
+    if (await this.isDeployed(chainId)) {
+      nonce = (await walletContract.getNonce(batchId)).toNumber()
+    }
+    console.log('nonce: ', nonce)
+
+    const gasEstimate1 = await this.ethersAdapter(chainId).estimateGas({
+      to: transaction.to,
+      data: transaction.data || '0x',
+      from: this.address,
+    });
+
+    // Depending on feeToken provide baseGas!
+
+    const baseGas = 25348 + 4928 + 16730 + 40015; //goerli USDC
+    const refundReceiver = "0x0000000000000000000000000000000000000000";
+    const gasToken = feeToken;
+    const gasPrice = "17"; // this would be token gas price
+    const targetTxGas = gasEstimate1; // would come from estimator
+
+    const walletTx: WalletTransaction = buildSmartAccountTransaction({
+      to: transaction.to,
+      value: transaction.value,
+      data: transaction.data, // for token transfers use encodeTransfer
+      targetTxGas,
+      baseGas,
+      refundReceiver,
+      gasPrice,
+      gasToken,
+      nonce
+    })
+
+    return walletTx
+  }
+
     /**
    * Prepares compatible WalletTransaction object based on Transaction Request
    * @todo Write test case and limit batch size based on test results in scw-contracts
