49e5b6bdeb4a3f3e3e5222395ca15fb1492b2750
livingrockrises <chirag@biconomy.io>
2022-08-16 18:36:01 +0530

    clear script

--- link.sh
+++ link.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-npm run unbuild
-npm run build
-npm link
+# npm run unbuild
+# npm run build
+# npm link
+rm -rf node_modules
+rm -rf packages/core-types/node_modules
+rm -rf packages/ethers-lib/node_modules
+rm -rf packages/node_client/node_modules
+rm -rf packages/relayer/node_modules
+rm -rf packages/smart-account/node_modules
+rm -rf packages/transactions/node_modules
