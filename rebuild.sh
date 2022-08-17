15660914f8f5fa512d5fb83e026d0a9ce3ee53b1
talha <talha.malik@ideofuzion.com>
2022-08-18 02:19:14 +0500

    script for making builds

---
+++ rebuild.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+rm -rf node_modules
+rm -rf packages/core-types/node_modules
+rm -rf packages/core-types/package-lock.json
+rm -rf packages/core-types/dist
+rm -rf packages/ethers-lib/node_modules
+rm -rf packages/ethers-lib/package-lock.json
+rm -rf packages/ethers-lib/dist
+
+rm -rf packages/node_client/node_modules
+rm -rf packages/node_client/package-lock.json
+rm -rf packages/node_client/dist
+
+rm -rf packages/relayer/node_modules
+rm -rf packages/relayer/package-lock.json
+rm -rf packages/relayer/dist
+
+rm -rf packages/smart-account/node_modules
+rm -rf packages/smart-account/package-lock.json
+rm -rf packages/smart-account/dist
+
+rm -rf packages/transactions/node_modules
+rm -rf packages/transactions/package-lock.json
+rm -rf packages/transactions/dist
+
+npx lerna bootstrap --force-local
+npm run build
+npm link
