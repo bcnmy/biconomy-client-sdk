5bbebb3924cba65e09d5b041212e1c0c8251685b
livingrockrises <chirag@biconomy.io>
2022-10-12 14:10:29 +0400

    replace axios with httpSendRequest

--- packages/account-abstraction/src/PaymasterAPI.ts
+++ packages/account-abstraction/src/PaymasterAPI.ts
@@ -1,17 +1,19 @@
 import { resolveProperties } from '@ethersproject/properties'
-import axios from 'axios' // httpSendRequest or through NodeClient
 import { UserOperation } from '@biconomy-sdk/core-types'
 import { hexConcat } from 'ethers/lib/utils'
+import { HttpMethod, sendRequest } from './utils/httpRequests'
 export class PaymasterAPI {
 
   // Might maintain API key at smart account level
   constructor(readonly apiUrl: string, readonly dappAPIKey: string) {
-    axios.defaults.baseURL = apiUrl
+    this.apiUrl = apiUrl
   }
 
   async getPaymasterAndData (userOp: Partial<UserOperation>): Promise<string> {
     console.log(userOp)
     userOp = await resolveProperties(userOp)
+    console.log('userOp')
+    console.log(userOp)
     // this.nodeClient.paymasterVerify()
     // Note: Might be different service that bypass SDK backend node
     userOp.nonce = Number(userOp.nonce)
@@ -23,16 +25,20 @@ export class PaymasterAPI {
     userOp.signature = '0x'
     userOp.paymasterAndData = '0x'
 
-    const result = await axios.post('/signing-service', {
-      userOp
+    const result: any = await  sendRequest({
+      url: `${this.apiUrl}/signing-service`,
+      method: HttpMethod.Post,
+      body: { "userOp": userOp }
     })
+
     console.log('******** ||||| *********')
     console.log('signing service response', result)
 
     // ToDo: Get paymaster addr from dapp id / smart account config
-    const paymasterAndData = hexConcat(['0x50e8996670759E1FAA315eeaCcEfe0c0A043aA51', result.data.signedMessage])
+    if(result) {
+    return hexConcat(['0x50e8996670759E1FAA315eeaCcEfe0c0A043aA51', result.signedMessage])
+    }
 
-    return  paymasterAndData
-    // return '0x'
+    return '0x'
   }
 }
