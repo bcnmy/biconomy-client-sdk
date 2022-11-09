1ff4af7b2a1e8781596334c3c147691408b31cd0
livingrockrises <chirag@biconomy.io>
2022-11-09 14:56:36 +0400

    case sensitive fix

---
+++ packages/ethers-lib/src/Types.ts
@@ -0,0 +1,13 @@
+import { ContractTransaction } from '@ethersproject/contracts'
+import { IBaseTransactionResult } from '@biconomy-sdk/core-types'
+
+export interface IEthersTransactionOptions {
+  from?: string
+  gasLimit?: number | string
+  gasPrice?: number | string
+}
+
+export interface IEthersTransactionResult extends IBaseTransactionResult {
+  transactionResponse: ContractTransaction
+  options?: IEthersTransactionOptions
+}
