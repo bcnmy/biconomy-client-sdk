badd18a710e0b9d7ae8ade9b4007717e9a9330de
amanraj1608 <archanaamanraj@gmail.com>
2022-12-16 12:51:04 +0530

    whitelabel solution and private key method

--- packages/web3-auth/src/types/Web3AuthConfig.ts
+++ packages/web3-auth/src/types/Web3AuthConfig.ts
@@ -2,8 +2,14 @@ export type DefaultSocialLoginConfig = {
   backendUrl: string
 }
 
+export type WhiteLabelDataType = {
+  name: string
+  logo: string
+}
+
 export type SocialLoginDTO = {
   chainId: string
   whitelistUrls: { [P in string]: string }
   network: 'mainnet' | 'testnet'
+  whteLableData: WhiteLabelDataType
 }
