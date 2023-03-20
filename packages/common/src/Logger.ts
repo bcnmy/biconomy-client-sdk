58c1cb5fb0094b4dea36ba601ae23bd4e0f298d8
amanraj1608 <archanaamanraj@gmail.com>
2023-03-21 04:47:15 +0530

    comment for readability

--- packages/common/src/Logger.ts
+++ packages/common/src/Logger.ts
@@ -9,12 +9,18 @@ class Logger {
 
   constructor() {
     // Check if the debug flag is set in the environment variables
-
     if (process.env.BICONOMY_SDK_DEBUG === 'true') {
       this.isDebug = true
     }
   }
 
+  /**
+   * \x1b[0m is an escape sequence to reset the color of the text
+   * All color codes used - 31 - Red, 33 - Yellow, 34 - Blue, 35 - Magenta, 36 - Cyan
+   * log - Magenta[time] Cyan[message]: Blue[value]
+   * warn - Magenta[time] Yellow[WARN]: Cyan[message] Yellow[value]
+   * error - Magenta[time] Red[ERROR]: Cyan[message] Red[value]
+   */
   log(message: string, value?: any): void {
     const timestamp = new Date().toISOString()
     const logMessage = `\x1b[35m[${timestamp}]\x1b[0m \x1b[36m${message}\x1b[0m:`
