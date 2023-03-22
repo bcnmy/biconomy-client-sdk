b31791a2defed6e2b6338744e9bb5fc26f123cba
talhamalik883 <talhamalik883@gmail.com>
2023-03-23 01:00:27 +0500

    fixes for EIP1271 changes

--- packages/core-types/src/contracts/FallbackGasTankContract.ts
+++ packages/core-types/src/contracts/FallbackGasTankContract.ts
@@ -5,8 +5,5 @@ import { BigNumber } from '@ethersproject/bignumber'
 export interface FallbackGasTankContract {
   getAddress(): string
   getContract(): Contract
-  getBalance(_dappIdentifier: string): Promise<BigNumber>
-  getNonce(_sender: string): Promise<BigNumber>
-  getHash(fallbackUserOp: any): Promise<string>
   getInterface(): Interface
 }
