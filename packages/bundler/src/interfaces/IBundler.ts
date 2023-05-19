79caa80738caa4d3db46bb578188a8accbf06f81
talhamalik883 <talhamalik883@gmail.com>
2023-05-19 18:19:50 +0500

    Review changes implementation

--- packages/bundler/src/interfaces/IBundler.ts
+++ packages/bundler/src/interfaces/IBundler.ts
@@ -2,9 +2,9 @@ import { UserOpResponse, UserOpGasFieldsResponse } from '../types/Types'
 import { ChainId, UserOperation } from '@biconomy/core-types'
 
 export interface IBundler {
-  getUserOpGasFields(
-    userOp: Partial<UserOperation>,
-    chainId: ChainId
-  ): Promise<UserOpGasFieldsResponse>
-  sendUserOp(userOp: UserOperation, chainId: ChainId): Promise<UserOpResponse>
+    getUserOpGasFields(
+        userOp: Partial<UserOperation>,
+        chainId: ChainId
+    ): Promise<UserOpGasFieldsResponse>
+    sendUserOp(userOp: UserOperation, chainId: ChainId): Promise<UserOpResponse>
 }
