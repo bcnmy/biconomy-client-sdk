4f9aafff4f0253ac8ef8416b0804f95a729a63a4
talhamalik883 <talhamalik883@gmail.com>
2023-05-19 19:52:12 +0500

    review changes for type and response type

--- packages/bundler/src/Bundler.ts
+++ packages/bundler/src/Bundler.ts
@@ -21,7 +21,7 @@ export class Bundler implements IBundler {
      * @description This function will fetch gasPrices from bundler
      * @returns Promise<UserOpGasPricesResponse>
      */
-    async getUserOpGasFields(userOp: Partial<UserOperation>, chainId: ChainId): Promise<UserOpGasPricesResponse> {
+    async getUserOpGasFields(userOp: Partial<UserOperation>, chainId: ChainId): Promise<UserOpGasFieldsResponse> {
         const response: any = await sendRequest({
             url: `${this.bundlerConfig.bundlerUrl}`,
             method: HttpMethod.Post,
