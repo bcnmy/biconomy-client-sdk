d2f5f1afadc2a561c4ef01c0821a25b9d7fe776e
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2023-05-24 03:59:38 +0530

    fix: linting

--- packages/bundler/src/Bundler.ts
+++ packages/bundler/src/Bundler.ts
@@ -1,58 +1,60 @@
-import { IBundler } from "./interfaces/IBundler";
+import { IBundler } from './interfaces/IBundler'
 import { UserOperation, ChainId } from '@biconomy/core-types'
-import { Bundlerconfig, UserOpResponse, UserOpGasFieldsResponse } from "./types/Types"
+import { Bundlerconfig, UserOpResponse, UserOpGasFieldsResponse } from './types/Types'
 import { resolveProperties } from 'ethers/lib/utils'
 import { deepHexlify, getTimestampInSeconds } from '@biconomy/common'
 import { HttpMethod, sendRequest } from './utils/httpRequests'
 
 /**
- * This class implements IBundler interface. 
- * Implementation sends UserOperation to a bundler URL as per ERC4337 standard. 
+ * This class implements IBundler interface.
+ * Implementation sends UserOperation to a bundler URL as per ERC4337 standard.
  * Checkout the proposal for more details on Bundlers.
  */
 export class Bundler implements IBundler {
+  constructor(readonly bundlerConfig: Bundlerconfig) {}
 
-    constructor(readonly bundlerConfig: Bundlerconfig) {}
+  /**
+   *
+   * @param chainId
+   * @description This function will fetch gasPrices from bundler
+   * @returns Promise<UserOpGasPricesResponse>
+   */
+  async getUserOpGasFields(
+    userOp: Partial<UserOperation>,
+    chainId: ChainId
+  ): Promise<UserOpGasFieldsResponse> {
+    const response: any = await sendRequest({
+      url: `${this.bundlerConfig.bundlerUrl}`,
+      method: HttpMethod.Post,
+      body: {
+        method: 'eth_getUserOpGasFields',
+        params: [userOp, this.bundlerConfig.entryPointAddress, chainId],
+        id: getTimestampInSeconds(),
+        jsonrpc: '2.0'
+      }
+    })
+    return response
+  }
+  /**
+   *
+   * @param userOp
+   * @description This function will send signed userOp to bundler to get mined on chain
+   * @returns Promise<UserOpResponse>
+   */
+  async sendUserOp(userOp: UserOperation, chainId: ChainId): Promise<UserOpResponse> {
+    const hexifiedUserOp = deepHexlify(await resolveProperties(userOp))
+    const params = [hexifiedUserOp, this.bundlerConfig.entryPointAddress, chainId]
 
-    /**
-     * 
-     * @param chainId 
-     * @description This function will fetch gasPrices from bundler
-     * @returns Promise<UserOpGasPricesResponse>
-     */
-    async getUserOpGasFields(userOp: Partial<UserOperation>, chainId: ChainId): Promise<UserOpGasFieldsResponse> {
-        const response: any = await sendRequest({
-            url: `${this.bundlerConfig.bundlerUrl}`,
-            method: HttpMethod.Post,
-            body: {
-                method: 'eth_getUserOpGasFields',
-                params: [userOp, this.bundlerConfig.entryPointAddress, chainId],
-                id: getTimestampInSeconds(),
-                jsonrpc: '2.0'
-            }
-        })
-        return response
-    }
-    /**
-     * 
-     * @param userOp 
-     * @description This function will send signed userOp to bundler to get mined on chain
-     * @returns Promise<UserOpResponse>
-     */
-    async sendUserOp(userOp: UserOperation, chainId: ChainId): Promise<UserOpResponse> {
-        const hexifiedUserOp = deepHexlify(await resolveProperties(userOp))
-        let params = [hexifiedUserOp, this.bundlerConfig.entryPointAddress, chainId]
-
-        const response: UserOpResponse = await sendRequest({
-            url: `${this.bundlerConfig.bundlerUrl}`,
-            method: HttpMethod.Post,
-            body: {
-                method: 'eth_sendUserOperation',
-                params: params,
-                id: getTimestampInSeconds(),
-                jsonrpc: '2.0'
-            }
-        })
-        return response
-    }
+    const response: UserOpResponse = await sendRequest({
+      url: `${this.bundlerConfig.bundlerUrl}`,
+      method: HttpMethod.Post,
+      body: {
+        method: 'eth_sendUserOperation',
+        params: params,
+        id: getTimestampInSeconds(),
+        jsonrpc: '2.0'
+      }
+    })
+    return response
+  }
 }
