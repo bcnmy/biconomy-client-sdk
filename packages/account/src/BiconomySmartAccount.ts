4d3487fd4133a053e55f00cd5a50d02b5847fcb7
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2023-07-20 16:11:25 +0400

    error handling for second estimation call to the bundler

--- packages/account/src/BiconomySmartAccount.ts
+++ packages/account/src/BiconomySmartAccount.ts
@@ -414,8 +414,15 @@ export class BiconomySmartAccount extends SmartAccount implements IBiconomySmart
         // Requesting to update gas limits again (especially callGasLimit needs to be re-calculated)
         delete finalUserOp.maxFeePerGas
         delete finalUserOp.maxPriorityFeePerGas
-        finalUserOp = await this.estimateUserOpGas(finalUserOp)
-        Logger.log('userOp after estimation ', finalUserOp)
+        try {
+          finalUserOp = await this.estimateUserOpGas(finalUserOp)
+          Logger.log('userOp after estimation ', finalUserOp)
+        } catch (error) {
+          Logger.error('Failed to estimate gas for userOp with updated callData ', error)
+          Logger.log(
+            'sending updated userOp. calculateGasLimit flaf should be sent to the paymaster to be able to update callGasLimit'
+          )
+        }
         return finalUserOp
       }
     } catch (error) {
