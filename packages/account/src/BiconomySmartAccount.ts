57ebc6a703a1b99cd644779bfddb9e25f8c179d6
talhamalik883 <talhamalik883@gmail.com>
2023-07-21 14:51:01 +0500

    verify account deployment even when nonce is 0

--- packages/account/src/BiconomySmartAccount.ts
+++ packages/account/src/BiconomySmartAccount.ts
@@ -288,10 +288,14 @@ export class BiconomySmartAccount extends SmartAccount implements IBiconomySmart
     } catch (error) {
       // Not throwing this error as nonce would be 0 if this.nonce() throw exception, which is expected flow for undeployed account
     }
+    let isDeployed = true
+
+    isDeployed = nonce.eq(0) ? await this.isAccountDeployed(this.address) : true
+ 
     let userOp: Partial<UserOperation> = {
       sender: this.address,
       nonce,
-      initCode: nonce.eq(0) ? this.initCode : '0x',
+      initCode: !isDeployed ? this.initCode : '0x',
       callData: callData
     }
 
