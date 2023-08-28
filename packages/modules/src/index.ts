62556179c6677dc5555f8b9ca22faff488e4432a
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2023-08-28 11:34:14 +0530

    update module addresses constants

--- packages/modules/src/index.ts
+++ packages/modules/src/index.ts
@@ -1,4 +1,5 @@
 export * from './utils/Types'
+export * from './utils/Constants'
 export * from './interfaces/IValidationModule'
 export * from './BaseValidationModule'
 export * from './ECDSAOwnershipValidationModule'
