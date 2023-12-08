547724a15366ee1e63aee80fdee0edc128a84c41
GabiDev <gv@popoo.io>
2023-12-08 17:06:12 +0200

    Added check for entryPointAddress field

--- packages/bundler/src/utils/Constants.ts
+++ packages/bundler/src/utils/Constants.ts
@@ -125,3 +125,5 @@ export const UserOpWaitForTxHashMaxDurationIntervals: { [key in ChainId]?: numbe
   [ChainId.CHILIZ_MAINNET]: 20000,
   [ChainId.CHILIZ_TESTNET]: 20000,
 };
+
+export const DEFAULT_ENTRYPOINT_ADDRESS = "0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789";
