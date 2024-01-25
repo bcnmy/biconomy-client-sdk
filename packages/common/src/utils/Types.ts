5aa3fbf0499a126021855fdecf68495419529eed
GabiDev <gv@popoo.io>
2024-01-25 17:50:02 +0200

    Refactor name

--- packages/common/src/utils/Types.ts
+++ packages/common/src/utils/Types.ts
@@ -3,9 +3,9 @@ import { Signer } from "@ethersproject/abstract-signer";
 import { SmartAccountSigner } from "@alchemy/aa-core";
 
 export type SupportedSignerName = "alchemy" | "ethers" | "viem";
-export type SupportedSigner = SmartAccountSigner | WalletClient | Signer | LightSigner;
+export type SupportedSigner = SmartAccountSigner | WalletClient | Signer | EthersV6Signer;
 
-export interface LightSigner {
+export interface EthersV6Signer {
   getAddress(): Promise<string>;
   signMessage(message: string | Uint8Array): Promise<string>;
 }
