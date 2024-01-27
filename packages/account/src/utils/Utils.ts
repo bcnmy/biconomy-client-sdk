8e2620efb0723076b049bacc2caec61d73990400
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2024-01-27 18:17:54 +0400

    merge conflicts

--- packages/account/src/utils/Utils.ts
+++ packages/account/src/utils/Utils.ts
@@ -1,5 +1,5 @@
 import { encodeAbiParameters, parseAbiParameters, keccak256, Hex } from "viem";
-import type { UserOperationStruct, BigNumberish, BytesLike } from "@biconomy/common";
+import type { UserOperationStruct } from "@biconomy/common";
 
 /**
  * pack the userOperation
