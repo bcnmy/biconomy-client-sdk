7dce1cc5733fd755a3376db145825fe70f938acf
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2024-01-28 16:12:37 +0400

    fix: error

--- packages/modules/tests/utils/customSession.ts
+++ packages/modules/tests/utils/customSession.ts
@@ -158,7 +158,7 @@ export class SessionFileStorage implements ISessionStorage {
     const client = createWalletClient({
       account: accountSigner,
       chain: signerData.chainId,
-      transport: http(polygonMumbai.rpcUrls.public.http[0]),
+      transport: http(polygonMumbai.rpcUrls.default.http[0]),
     });
     const walletClientSigner: SmartAccountSigner = new WalletClientSigner(
       client,
@@ -185,7 +185,7 @@ export class SessionFileStorage implements ISessionStorage {
     const signer = privateKeyToAccount(signerData.pvKey);
     const walletClient = createWalletClient({
       account: signer,
-      transport: http(polygonMumbai.rpcUrls.public.http[0]),
+      transport: http(polygonMumbai.rpcUrls.default.http[0]),
     });
     return new WalletClientSigner(walletClient, "json-rpc");
   }
