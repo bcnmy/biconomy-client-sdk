3d3f12c177909ca2ea2fcaee52cdf058b27e5c76
Joe Pegler <joepegler123@gmail.com>
2024-02-13 16:19:06 +0000

    Update module types for jsdoc

--- packages/modules/src/utils/Types.ts
+++ packages/modules/src/utils/Types.ts
@@ -6,39 +6,51 @@ import { SupportedSigner } from "@biconomy/common";
 export type ModuleVersion = "V1_0_0"; // | 'V1_0_1'
 
 export interface BaseValidationModuleConfig {
+  /** entryPointAddress: address of the entry point */
   entryPointAddress?: Hex;
 }
 
 export interface ECDSAOwnershipValidationModuleConfig extends BaseValidationModuleConfig {
+  /** Address of the module */
   moduleAddress?: Hex;
+  /** Version of the module */
   version?: ModuleVersion;
+  /** Signer: viemWallet or ethers signer. Ingested when passed into smartAccount */
   signer: SupportedSigner;
 }
 
 export interface ECDSAOwnershipValidationModuleConfigConstructorProps extends BaseValidationModuleConfig {
+  /** Address of the module */
   moduleAddress?: Hex;
+  /** Version of the module */
   version?: ModuleVersion;
-  signer: SmartAccountSigner;
+  /** Signer: viemWallet or ethers signer. Ingested when passed into smartAccount */
+  signer: SupportedSigner;
 }
 
 export interface SessionKeyManagerModuleConfig extends BaseValidationModuleConfig {
+  /** Address of the module */
   moduleAddress?: Hex;
+  /** Version of the module */
   version?: ModuleVersion;
-  nodeClientUrl?: string;
+  /** SmartAccount address */
   smartAccountAddress: string;
   storageType?: StorageType;
   sessionStorageClient?: ISessionStorage;
 }
 
 export interface BatchedSessionRouterModuleConfig extends BaseValidationModuleConfig {
+  /** Address of the module */
   moduleAddress?: Hex;
+  /** Version of the module */
   version?: ModuleVersion;
-
-  sessionKeyManagerModule?: SessionKeyManagerModule; // could be BaseValidationModule
-
+  /** Session Key Manager module: Could be BaseValidationModule */
+  sessionKeyManagerModule?: SessionKeyManagerModule;
+  /** Session Key Manager module address */
   sessionManagerModuleAddress?: Hex;
-  nodeClientUrl?: string;
+  /** Address of the associated smart account */
   smartAccountAddress: string;
+  /** Storage type, e.g. local storage */
   storageType?: StorageType;
 }
 
@@ -47,9 +59,13 @@ export enum StorageType {
 }
 
 export type SessionParams = {
+  /** Redundant now as we've favoured uuid() */
   sessionID?: string;
+  /** Session Signer: viemWallet or ethers signer. Ingested when passed into smartAccount */
   sessionSigner: SupportedSigner;
+  /** The session validation module which contains the logic on how to decode the session call data. for example erc20 session validation validates the amount of erc20 transfer */
   sessionValidationModule?: Hex;
+  /** Additional info if needed to be appended in signature */
   additionalSessionData?: string;
 };
 
@@ -57,21 +73,28 @@ export type ModuleInfo = {
   // Could be a full object of below params and that way it can be an array too!
   // sessionParams?: SessionParams[] // where SessionParams is below four
   sessionID?: string;
+  /** Session Signer: viemWallet or ethers signer. Ingested when passed into smartAccount */
   sessionSigner?: SupportedSigner;
+  /** The session validation module which contains the logic on how to decode the session call data. for example erc20 session validation validates the amount of erc20 transfer */
   sessionValidationModule?: Hex;
+  /** Additional info if needed to be appended in signature */
   additionalSessionData?: string;
   batchSessionParams?: SessionParams[];
 };
 
 export interface SendUserOpParams extends ModuleInfo {
+  /** Type of similation: validation or validation_and_execution */
   simulationType?: SimulationType;
 }
 
 export type SimulationType = "validation" | "validation_and_execution";
 
 export type SignerData = {
+  /** Public key */
   pbKey: string;
+  /** Private key */
   pvKey: `0x${string}`;
+  /** Network Id */
   chainId?: Chain;
 };
 
@@ -81,27 +104,38 @@ export type CreateSessionDataResponse = {
 };
 
 export interface CreateSessionDataParams {
+  /** window end for the session key */
   validUntil: number;
+  /** window start for the session key */
   validAfter: number;
   sessionValidationModule: Hex;
   sessionPublicKey: Hex;
   sessionKeyData: Hex;
+  /** if multiple session keys are there can pass the uuid here to filter that session key EOA */
   preferredSessionId?: string;
 }
 
 export interface MultiChainValidationModuleConfig extends BaseValidationModuleConfig {
+  /** Address of the module */
   moduleAddress?: Hex;
+  /** Version of the module */
   version?: ModuleVersion;
+  /** Signer: viemWallet or ethers signer. Ingested when passed into smartAccount */
   signer: SupportedSigner;
 }
 export interface MultiChainValidationModuleConfigConstructorProps extends BaseValidationModuleConfig {
+  /** Address of the module */
   moduleAddress?: Hex;
+  /** Version of the module */
   version?: ModuleVersion;
-  signer: SmartAccountSigner;
+  /** Signer: viemWallet or ethers signer. Ingested when passed into smartAccount */
+  signer: SupportedSigner;
 }
 
 export type MultiChainUserOpDto = {
+  /** window end timestamp */
   validUntil?: number;
+  /** window start timestamp */
   validAfter?: number;
   chainId: number;
   userOp: Partial<UserOperationStruct>;
@@ -112,11 +146,15 @@ export interface BaseSessionKeyData {
 }
 
 export interface ERC20SessionKeyData extends BaseSessionKeyData {
+  /** ERC20 token address */
   token: Hex;
+  /** Recipient address */
   recipient: Hex;
+  /** ERC20 amount (Bigint) */
   maxAmount: bigint;
 }
 
 export interface SessionValidationModuleConfig {
+  /** Address of the module */
   moduleAddress: string;
 }
