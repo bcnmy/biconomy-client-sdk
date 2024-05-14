b9b2077b8a368456b51bf6b9b7af5bb79fffc072
joepegler <joepegler123@gmail.com>
2024-05-14 13:16:02 +0100

    chore: sessions dx (#486)
    * feat: SMA-792 sessions devex

---
+++ src/modules/sessions/batch.ts
@@ -0,0 +1,232 @@
+import type { Chain, Hex } from "viem"
+import {
+  type CreateSessionDataParams,
+  DEFAULT_ABI_SVM_MODULE,
+  DEFAULT_BATCHED_SESSION_ROUTER_MODULE,
+  DEFAULT_SESSION_KEY_MANAGER_MODULE,
+  MODULE_ADDRESSES,
+  type Session,
+  type SessionGrantedPayload,
+  type SessionParams,
+  createBatchedSessionRouterModule,
+  createSessionKeyManagerModule
+} from ".."
+import {
+  type BiconomySmartAccountV2,
+  type BuildUserOpOptions,
+  ERROR_MESSAGES,
+  type Transaction
+} from "../../account"
+import type { ISessionStorage } from "../interfaces/ISessionStorage"
+
+export type CreateBatchSessionConfig = {
+  /** The storage client to be used for storing the session data */
+  sessionStorageClient: ISessionStorage
+  /** An array of session configurations */
+  leaves: CreateSessionDataParams[]
+}
+
+/**
+ *
+ * createBatchSession
+ *
+ * Creates a session manager that handles multiple sessions at once for a given user's smart account.
+ * Useful for handling multiple granted sessions at once.
+ *
+ * @param smartAccount - The user's {@link BiconomySmartAccountV2} smartAccount instance.
+ * @param sessionKeyAddress - The address of the sessionKey upon which the policy is to be imparted.
+ * @param batchSessionConfig - An array of session configurations {@link CreateBatchSessionConfig}.
+ * @param buildUseropDto - Optional. {@link BuildUserOpOptions}
+ * @returns Promise<{@link SessionGrantedPayload}> - An object containing the status of the transaction and the sessionID.
+ *
+ * @example
+ *
+ * ```typescript
+ * import { createClient } from "viem"
+ * import { createSmartAccountClient } from "@biconomy/account"
+ * import { createWalletClient, http } from "viem";
+ * import { polygonAmoy } from "viem/chains";
+ *
+ * const signer = createWalletClient({
+ *   account,
+ *   chain: polygonAmoy,
+ *   transport: http(),
+ * });
+ *
+ * const smartAccount = await createSmartAccountClient({ signer, bundlerUrl, paymasterUrl }); // Retrieve bundler/paymaster url from dashboard
+ * const smartAccountAddress = await smartAccount.getAccountAddress();
+ * const nftAddress = "0x1758f42Af7026fBbB559Dc60EcE0De3ef81f665e"
+ * const sessionStorage = new SessionFileStorage(smartAccountAddress);
+ * const sessionKeyAddress = (await sessionStorage.addSigner(undefined, polygonAmoy)).getAddress();
+ *
+ *  const leaves: CreateSessionDataParams[] = [
+ *    createERC20SessionDatum({
+ *      interval: {
+ *        validUntil: 0,
+ *        validAfter: 0
+ *      },
+ *      sessionKeyAddress,
+ *      sessionKeyData: encodeAbiParameters(
+ *        [
+ *          { type: "address" },
+ *          { type: "address" },
+ *          { type: "address" },
+ *          { type: "uint256" }
+ *        ],
+ *        [sessionKeyAddress, token, recipient, amount]
+ *      )
+ *    }),
+ *    createABISessionDatum({
+ *      interval: {
+ *        validUntil: 0,
+ *        validAfter: 0
+ *      },
+ *      sessionKeyAddress,
+ *      contractAddress: nftAddress,
+ *      functionSelector: "safeMint(address)",
+ *      rules: [
+ *        {
+ *          offset: 0,
+ *          condition: 0,
+ *          referenceValue: smartAccountAddress
+ *        }
+ *      ],
+ *      valueLimit: 0n
+ *    })
+ *  ]
+ *
+ *  const { wait, sessionID } = await createBatchSession(
+ *    smartAccount,
+ *    sessionKeyAddress,
+ *    sessionStorageClient: sessionStorage,
+ *    leaves,
+ *    {
+ *      paymasterServiceData: { mode: PaymasterMode.SPONSORED },
+ *    }
+ *  )
+ *
+ *  const {
+ *    receipt: { transactionHash },
+ *    success
+ *  } = await wait();
+ *
+ *  console.log({ sessionID, success }); // Use the sessionID later to retrieve the sessionKey from the storage client
+ *
+ * ```
+ */
+
+export const createBatchSession = async (
+  smartAccount: BiconomySmartAccountV2,
+  sessionKeyAddress: Hex,
+  /** The storage client to be used for storing the session data */
+  sessionStorageClient: ISessionStorage,
+  /** An array of session configurations */
+  leaves: CreateSessionDataParams[],
+  buildUseropDto?: BuildUserOpOptions
+): Promise<SessionGrantedPayload> => {
+  const userAccountAddress = await smartAccount.getAddress()
+
+  const sessionsModule = await createSessionKeyManagerModule({
+    smartAccountAddress: userAccountAddress,
+    sessionStorageClient
+  })
+
+  // Create batched session module
+  const batchedSessionModule = await createBatchedSessionRouterModule({
+    smartAccountAddress: userAccountAddress,
+    sessionKeyManagerModule: sessionsModule
+  })
+
+  const { data: policyData } =
+    await batchedSessionModule.createSessionData(leaves)
+
+  const permitTx = {
+    to: DEFAULT_SESSION_KEY_MANAGER_MODULE,
+    data: policyData
+  }
+
+  const isDeployed = await smartAccount.isAccountDeployed()
+  if (!isDeployed) throw new Error(ERROR_MESSAGES.ACCOUNT_NOT_DEPLOYED)
+
+  const txs: Transaction[] = []
+  const [isSessionModuleEnabled, isBatchedSessionModuleEnabled] =
+    await Promise.all([
+      smartAccount.isModuleEnabled(DEFAULT_SESSION_KEY_MANAGER_MODULE),
+      smartAccount.isModuleEnabled(DEFAULT_BATCHED_SESSION_ROUTER_MODULE)
+    ])
+
+  if (!isSessionModuleEnabled) {
+    txs.push(
+      await smartAccount.getEnableModuleData(DEFAULT_SESSION_KEY_MANAGER_MODULE)
+    )
+  }
+  if (!isBatchedSessionModuleEnabled) {
+    txs.push(
+      await smartAccount.getEnableModuleData(
+        DEFAULT_BATCHED_SESSION_ROUTER_MODULE
+      )
+    )
+  }
+  txs.push(permitTx)
+
+  const userOpResponse = await smartAccount.sendTransaction(txs, buildUseropDto)
+
+  const sessionID =
+    (
+      await sessionStorageClient.getSessionData({
+        sessionPublicKey: sessionKeyAddress,
+        sessionValidationModule: DEFAULT_ABI_SVM_MODULE
+      })
+    ).sessionID ?? ""
+
+  return {
+    session: {
+      sessionStorageClient,
+      sessionID
+    },
+    ...userOpResponse
+  }
+}
+
+const types = ["ERC20", "ABI"] as const
+export type BatchSessionParamsPayload = {
+  params: { batchSessionParams: SessionParams[] }
+}
+export type SessionValidationType = (typeof types)[number]
+/**
+ * getBatchSessionTxParams
+ *
+ * Retrieves the transaction parameters for a batched session.
+ *
+ * @param sessionTypes - An array of session types.
+ * @param transactions - An array of {@link Transaction}s.
+ * @param session - {@link Session}.
+ * @param chain - The chain.
+ * @returns Promise<{@link BatchSessionParamsPayload}> - session parameters.
+ *
+ */
+export const getBatchSessionTxParams = async (
+  sessionValidationTypes: SessionValidationType[],
+  transactions: Transaction[],
+  { sessionID, sessionStorageClient }: Session,
+  chain: Chain
+): Promise<BatchSessionParamsPayload> => {
+  if (sessionValidationTypes.length !== transactions.length) {
+    throw new Error(ERROR_MESSAGES.INVALID_SESSION_TYPES)
+  }
+  const sessionSigner = await sessionStorageClient.getSignerBySession(
+    {
+      sessionID
+    },
+    chain
+  )
+
+  return {
+    params: {
+      batchSessionParams: sessionValidationTypes.map((sessionType) => ({
+        sessionSigner,
+        sessionValidationModule: MODULE_ADDRESSES[sessionType]
+      }))
+    }
+  }
+}
---
+++ src/modules/sessions/sessionSmartAccountClient.ts
@@ -0,0 +1,119 @@
+import { http, type Hex, createWalletClient } from "viem"
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
+import {
+  createBatchedSessionRouterModule,
+  createSessionKeyManagerModule
+} from ".."
+import {
+  type BiconomySmartAccountV2,
+  type BiconomySmartAccountV2Config,
+  createSmartAccountClient,
+  getChain
+} from "../../account"
+import type { ModuleInfo } from "../utils/Types"
+import type { Session } from "./abi"
+
+export type ImpersonatedSmartAccountConfig = Omit<
+  BiconomySmartAccountV2Config,
+  "signer"
+> & {
+  accountAddress: Hex
+  chainId: number
+  bundlerUrl: string
+}
+/**
+ *
+ * createSessionSmartAccountClient
+ *
+ * Creates a new instance of BiconomySmartAccountV2 class. This is used to impersonate a users smart account by a dapp, for use
+ * with a valid session that has previously been granted by the user. A dummy signer is passed into the smart account instance, which cannot be used.
+ * The sessionSigner is used instead for signing transactions, which is fetched from the session storage using the sessionID. {@link ISessionStorage}
+ *
+ * @param biconomySmartAccountConfig - Configuration for initializing the BiconomySmartAccountV2 instance {@link ImpersonatedSmartAccountConfig}.
+ * @returns A promise that resolves to a new instance of {@link BiconomySmartAccountV2}.
+ * @throws An error if something is wrong with the smart account instance creation.
+ *
+ * @example
+ * import { createClient } from "viem"
+ * import { createSmartAccountClient, BiconomySmartAccountV2 } from "@biconomy/account"
+ * import { createWalletClient, http } from "viem";
+ * import { polygonAmoy } from "viem/chains";
+ *
+ * const signer = createWalletClient({
+ *   account,
+ *   chain: polygonAmoy,
+ *   transport: http(),
+ * });
+ *
+ *
+ * // The following fields are required to create a session smart account client
+ * const smartAccountAddress = '0x...';
+ * const sessionStorage = new SessionFileStorage(smartAccountAddress);
+ * const sessionKeyAddress = '0x...';
+ * const sessionID = '0x...';
+ *
+ * const smartAccountWithSession = await createSessionSmartAccountClient(
+ *   {
+ *     accountAddress: smartAccountAddress, // Set the account address on behalf of the user
+ *     bundlerUrl,
+ *     paymasterUrl,
+ *     chainId
+ *   },
+ *   {
+ *     sessionStorageClient: storeForSingleSession,
+ *     sessionID
+ *   }
+ * )
+ *
+ * // The smartAccountWithSession instance can now be used to interact with the blockchain on behalf of the user in the same manner as a regular smart account instance.
+ * // smartAccountWithSession.sendTransaction(...) etc.
+ *
+ */
+export const createSessionSmartAccountClient = async (
+  biconomySmartAccountConfig: ImpersonatedSmartAccountConfig,
+  { sessionStorageClient, sessionID }: Session,
+  multiMode = false
+): Promise<BiconomySmartAccountV2> => {
+  const account = privateKeyToAccount(generatePrivateKey())
+
+  const chain =
+    biconomySmartAccountConfig.viemChain ??
+    getChain(biconomySmartAccountConfig.chainId)
+
+  const incompatibleSigner = createWalletClient({
+    account,
+    chain,
+    transport: http()
+  })
+
+  const sessionSigner = await sessionStorageClient.getSignerBySession(
+    {
+      sessionID
+    },
+    chain
+  )
+
+  const sessionData: ModuleInfo | undefined = multiMode
+    ? undefined
+    : {
+        sessionID,
+        sessionSigner
+      }
+
+  const sessionModule = await createSessionKeyManagerModule({
+    smartAccountAddress: biconomySmartAccountConfig.accountAddress,
+    sessionStorageClient
+  })
+
+  const batchedSessionModule = await createBatchedSessionRouterModule({
+    smartAccountAddress: biconomySmartAccountConfig.accountAddress,
+    sessionKeyManagerModule: sessionModule
+  })
+
+  return await createSmartAccountClient({
+    ...biconomySmartAccountConfig,
+    signer: incompatibleSigner, // This is a dummy signer, it will remain unused
+    activeValidationModule: multiMode ? batchedSessionModule : sessionModule,
+    sessionData // contains the sessionSigner that will be used for txs
+  })
+}
