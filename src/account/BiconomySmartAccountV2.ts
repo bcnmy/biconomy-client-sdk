897d37a91d7d2ccaf3dfae7d51998f2b88750020
Joe Pegler <joepegler123@gmail.com>
2024-06-26 11:46:55 +0100

    fix: rpcUrl

--- src/account/BiconomySmartAccountV2.ts
+++ src/account/BiconomySmartAccountV2.ts
@@ -204,7 +204,7 @@ export class BiconomySmartAccountV2 extends BaseSmartContractAccount {
         getChain(biconomySmartAccountConfig.chainId),
       transport: http(
         biconomySmartAccountConfig.rpcUrl ||
-          getChain(biconomySmartAccountConfig.chainId).rpcUrls.default.http[0]
+        getChain(biconomySmartAccountConfig.chainId).rpcUrls.default.http[0]
       )
     })
 
@@ -251,7 +251,7 @@ export class BiconomySmartAccountV2 extends BaseSmartContractAccount {
     biconomySmartAccountConfig: BiconomySmartAccountV2Config
   ): Promise<BiconomySmartAccountV2> {
     let chainId = biconomySmartAccountConfig.chainId
-    let rpcUrl = biconomySmartAccountConfig.rpcUrl
+    let rpcUrl = biconomySmartAccountConfig.customChain?.rpcUrls?.default?.http?.[0] ?? biconomySmartAccountConfig.rpcUrl
     let resolvedSmartAccountSigner!: SmartAccountSigner
 
     // Signer needs to be initialised here before defaultValidationModule is set
